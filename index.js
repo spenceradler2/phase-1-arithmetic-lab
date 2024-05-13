ddb7cc7ad9542a98d900e177a59b92c62f90c355
Spencer <spencer.adler2@gmail.com>
2024-05-13 19:47:12 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31 
+const num2 = 2 
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 8
+const num4  = 2
+
+const mod = num3/num4
+
+const max = Math.max(1, 2, 3, 4, 20);
